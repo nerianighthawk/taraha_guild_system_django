7b7572f36df6536022ede9f18b185f6545e766c9
nerianighthawk <hiroya.nakakawaji@gmail.com>
2020-07-26 13:53:31 +0900

    create to post discord api.

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from rest.views import EventViewSet, Participation
+from rest.views import EventViewSet, Participation, Discord
 
 urlpatterns = [
     url(r'^event/$', EventViewSet.as_view({'get': 'list', 'post': 'create'})),
+    url(r'^event/(?P<eid>[0-9]+)/$', EventViewSet.as_view({'delete': 'destroy'})),
     url(r'^participation/$', Participation.as_view()),
+    url(r'^discord/$', Discord.as_view()),
 ]
--- rest/views.py
+++ rest/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest.models import Event, Participant
 from rest.serializer import EventSerializer, ParticipantSerializer
+import requests
+import json
+from datetime import datetime
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -51,8 +54,7 @@ class EventViewSet(viewsets.ModelViewSet):
         return Response(data=event.id, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
-        data = request.data
-        eid = data.get('id')
+        eid = self.kwargs['eid']
         event = self.queryset.filter(id=eid)
         event.delete()
         return Response(data={}, status=status.HTTP_200_OK)
@@ -64,10 +66,57 @@ class Participation(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         data = request.data
-        print(data)
         event_id = data.get('event')
         event = Event.objects.filter(id=event_id).get()
         name = data.get('name')
         participant = Participant(event=event, name=name)
         participant.save()
         return Response(data=participant.id, status=status.HTTP_201_CREATED)
+
+
+class Discord(generics.RetrieveAPIView):
+    queryset = Event.objects
+    serializer_class = EventSerializer
+
+    @staticmethod
+    def create_query(event: Event):
+        return {
+            'title': event.title,
+            'fields': [
+                {
+                    'name': '日時',
+                    'value': event.date.strftime('%Y年%m月%d日 %H:%M')
+                },
+                {
+                    'name': '場所',
+                    'value': event.place
+                },
+                {
+                    'name': '備考',
+                    'value': event.remark
+                },
+            ]
+        }
+
+    def get(self, request, *args, **kwargs):
+        today = datetime.now()
+        all_events = self.queryset.all()
+        events = list(filter(lambda e: today.date() == e.date.date(), all_events))
+        url = "https://discordapp.com/api/webhooks/736767432076689471/bI2PyBoHm02ur00VU1vtXri5Fk_daEGc2yBR0-0QkUO65Xnukmf7UMHEutt-xAFhXswl"
+        headers = {'content-type': 'application/json'}
+
+        if len(events) == 0:
+            query = {
+                'content': '本日のイベントはありません。'
+            }
+            payload = json.dumps(query)
+            requests.post(url, data=payload, headers=headers)
+        else:
+            query = {
+                'content': '本日のイベントです。',
+                'embeds': [self.create_query(e) for e in events],
+            }
+            payload = json.dumps(query)
+            requests.post(url, data=payload, headers=headers)
+
+        return Response(data=query, status=status.HTTP_200_OK)
